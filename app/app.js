72760a3c6a8b201b223c885cd50f3baa5e0066d8
christian vazquez <christianvazquezit@gmail.com>
2016-11-09 12:30:05 -0600

    app structure

---
+++ app/app.js
@@ -0,0 +1,6 @@
+(function () {
+    // All angular components should be defined under an IIFE. See: https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+    // Try to keep the name of your main module as short as possible while being descriptive.
+    // Define in this file the dependencies for your Angular module.
+    angular.module('pr270', []);
+}());
