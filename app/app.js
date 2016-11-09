f4718a1c819c4e3137c8959a31bcf52c818007e4
christian vazquez <christianvazquezit@gmail.com>
2016-11-09 12:58:48 -0600

    I added npm configuration

--- app/app.js
+++ app/app.js
@@ -2,5 +2,5 @@
     // All angular components should be defined under an IIFE. See: https://developer.mozilla.org/en-US/docs/Glossary/IIFE
     // Try to keep the name of your main module as short as possible while being descriptive.
     // Define in this file the dependencies for your Angular module.
-    angular.module('pr270', []);
+    angular.module('pr270', ['ui.router']);
 }());
---
+++ app/routes.js
@@ -0,0 +1,22 @@
+(function () {
+    angular.module('pr270').config(configRoutes);
+
+    configRoutes.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function configRoutes($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('dashboardIndex', {
+                url: '/dashboard',
+                views: {
+                    'content': {
+                        controller: 'DashboardController',
+                        controllerAs: 'vm',
+                        templateUrl: 'ng-html/components/dashboard/dashboard.view.html'
+                    }
+                }
+            });
+
+
+        $urlRouterProvider.otherwise('/dashboard');
+    }
+}());
